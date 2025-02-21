fcf2a105e26774bcd5983b73cbd8ebafa908166d
Shiru <nckim73@gmail.com>
2025-02-21 13:25:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ body.innerHTML += "<h2> Basic Properties </h2>";
 const basic_table = document.createElement("table");
 basic_table.innerHTML = `
 <thead>
-<tr> <td colspan="3"> Orbital Properties </td> </tr>
+<tr  class="title"> <td colspan="3"> Orbital Properties </td> </tr>
 <tr> 
 	<td> Property </td>
 	<td> Earth </td>
@@ -51,7 +51,7 @@ basic_table.innerHTML = `
 	<td> ${earth_periapsis} </td>
 	<td> ${moon_periapsis} </td>
 </tr>
-<tr>
+<tr class="even">
 	<td> Apoapsis (km) </td>
 	<td> ${earth_apoapsis} </td>
 	<td> ${moon_apoapsis} </td>
@@ -61,7 +61,7 @@ basic_table.innerHTML = `
 	<td> ${earth_period} </td>
 	<td> ${moon_period} </td>
 </tr>
-<tr>
+<tr class="even">
 	<td> Synodic Period (days) </td>
 	<td> - </td>
 	<td> ${moon_synodic} </td>
@@ -77,14 +77,14 @@ body.appendChild(basic_table);
 
 body.innerHTML += "<h2> Calendar Properties </h2>";
 const solar_calendar_table = document.createElement("table");
-solar_calendar_table.innerHTML = `
+let text = `
 <thead>
-<tr> <td colspan="2"> Solar Calendar Properties </td> </tr>
+<tr  class="title"> <td colspan="2"> Solar Calendar Properties </td> </tr>
 <tr> 
 	<td> Days in Year </td>
 	<td> ${Math.floor(earth_period)} </td>
 </tr>
-<tr> <td colspan="2"> </td> </tr>
+<tr > <td colspan="2"> </td> </tr>
 <tr> 
 	<td> Leap Days per Years </td>
 	<td> Error (years/1day) </td>
@@ -93,22 +93,27 @@ solar_calendar_table.innerHTML = `
 <tbody>
 `;
 
-var solar_leaps = dec_to_frac(earth_period);
+var solar_leaps = dec_to_frac(earth_period, 10);
 for (let i = 0; i < solar_leaps.length; i++) {
-	solar_calendar_table.innerHTML += "<tr> <td>" + solar_leaps[i][0].toString() + " / " + solar_leaps[i][1].toString() + "</td> <td>" + (1/(Math.floor(earth_period) + solar_leaps[i][0]/solar_leaps[i][1] - earth_period)).toString() + "</td> </tr>";
+	text += "<tr ";
+	if (i % 2 == 1) {
+		text += "class='even'"
+	}
+	text += "> <td>" + solar_leaps[i][0].toString() + " / " + solar_leaps[i][1].toString() + "</td> <td>" + (1/(Math.floor(earth_period) + solar_leaps[i][0]/solar_leaps[i][1] - earth_period)).toString() + "</td> </tr>";
+	solar_calendar_table.innerHTML = text;
 }
 solar_calendar_table.innerHTML += "</tbody>";
 body.appendChild(solar_calendar_table);
 
 const lunar_calendar_table = document.createElement("table");
-lunar_calendar_table.innerHTML = `
+text = `
 <thead>
-<tr> <td colspan="2"> Lunar Calendar Properties </td> </tr>
-<tr> 
+<tr  class="title"> <td colspan="2"> Lunar Calendar Properties </td> </tr>
+<tr > 
 	<td> Days in Short Month </td>
 	<td> ${Math.floor(moon_synodic)} </td>
 </tr>
-<tr> 
+<tr > 
 	<td> Months in Year </td>
 	<td> ${Math.floor(months_in_year)} </td>
 </tr>
@@ -125,23 +130,28 @@ lunar_calendar_table.innerHTML = `
 <tbody>
 `;
 
-var lunar_leaps = dec_to_frac(moon_synodic);
+var lunar_leaps = dec_to_frac(moon_synodic, 10);
 for (let i = 0; i < lunar_leaps.length; i++) {
-	lunar_calendar_table.innerHTML += "<tr> <td>" + lunar_leaps[i][0].toString() + " / " + lunar_leaps[i][1].toString() + "</td> <td>" + (1/(Math.floor(moon_synodic) + lunar_leaps[i][0]/lunar_leaps[i][1] - moon_synodic)).toString() + "</td> </tr>";
+	text += "<tr ";
+	if (i % 2 == 1) {
+		text += "class='even'"
+	}
+	text += "> <td>" + lunar_leaps[i][0].toString() + " / " + lunar_leaps[i][1].toString() + "</td> <td>" + (1/(Math.floor(moon_synodic) + lunar_leaps[i][0]/lunar_leaps[i][1] - moon_synodic)).toString() + "</td> </tr>";
+	lunar_calendar_table.innerHTML = text;
 }
 
 lunar_calendar_table.innerHTML += "</tbody>";
 body.appendChild(lunar_calendar_table);
 
 const lunisolar_calendar_table = document.createElement("table");
-lunisolar_calendar_table.innerHTML = `
+text = `
 <thead>
-<tr> <td colspan="2"> Lunisolar Calendar Properties </td> </tr>
-<tr> 
+<tr  class="title"> <td colspan="2"> Lunisolar Calendar Properties </td> </tr>
+<tr > 
 	<td> Days in Short Month </td>
 	<td> ${Math.floor(moon_synodic)} </td>
 </tr>
-<tr> 
+<tr > 
 	<td> Months in Year </td>
 	<td> ${Math.floor(months_in_year)} </td>
 </tr>
@@ -154,9 +164,14 @@ lunisolar_calendar_table.innerHTML = `
 <tbody>
 `;
 
-var lunisolar_leaps = dec_to_frac(months_in_year);
+var lunisolar_leaps = dec_to_frac(months_in_year, 10);
 for (let i = 0; i < lunisolar_leaps.length; i++) {
-	lunisolar_calendar_table.innerHTML += "<tr> <td>" + lunisolar_leaps[i][0].toString() + " / " + lunisolar_leaps[i][1].toString() + "</td> <td>" + (1/(Math.floor(months_in_year) + lunisolar_leaps[i][0]/lunisolar_leaps[i][1] - months_in_year)).toString() + "</td> </tr>";
+	text += "<tr ";
+	if (i % 2 == 1) {
+		text += "class='even'"
+	}
+	text += "> <td>" + lunisolar_leaps[i][0].toString() + " / " + lunisolar_leaps[i][1].toString() + "</td> <td>" + (1/(Math.floor(months_in_year) + lunisolar_leaps[i][0]/lunisolar_leaps[i][1] - months_in_year)).toString() + "</td> </tr>";
+	lunisolar_calendar_table.innerHTML = text;
 }
 lunisolar_calendar_table.innerHTML += "</tbody>";
 body.appendChild(lunisolar_calendar_table);
@@ -167,7 +182,7 @@ var seasons = calculate_seasons();
 const solar_terms_table = document.createElement("table");
 solar_terms_table.innerHTML = `
 <thead>
-<tr> <td colspan="3"> Solar Terms </td> </tr>
+<tr  class="title"> <td colspan="3"> Solar Terms </td> </tr>
 <tr> 
 	<td> Northern Term </td>
 	<td> Days after Periapsis </td>
@@ -180,7 +195,7 @@ solar_terms_table.innerHTML = `
 	<td> ${seasons[0][0]} </td>
 	<td> Liqiu / Lunasa </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Spring Equinox </td>
 	<td> ${seasons[0][1]} </td>
 	<td> Autumn Equinox </td>
@@ -190,7 +205,7 @@ solar_terms_table.innerHTML = `
 	<td> ${seasons[0][2]} </td>
 	<td> Lidong / Samhain </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Summer Solstice </td>
 	<td> ${seasons[0][3]} </td>
 	<td> Winter Solstice </td>
@@ -200,7 +215,7 @@ solar_terms_table.innerHTML = `
 	<td> ${seasons[0][4]} </td>
 	<td> Lichun / Imbolc </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Autumn Equinox </td>
 	<td> ${seasons[0][5]} </td>
 	<td> Spring Equinox </td>
@@ -210,7 +225,7 @@ solar_terms_table.innerHTML = `
 	<td> ${seasons[0][6]} </td>
 	<td> Lixia / Bealtaine </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Winter Solstice </td>
 	<td> ${seasons[0][7]} </td>
 	<td> Summer Solstice </td>
@@ -222,7 +237,7 @@ body.appendChild(solar_terms_table);
 const astro_seasons_table = document.createElement("table");
 astro_seasons_table.innerHTML = `
 <thead>
-<tr> <td colspan="3"> Lengths of Astronomical Seasons </td> </tr>
+<tr  class="title"> <td colspan="3"> Lengths of Astronomical Seasons </td> </tr>
 <tr> 
 	<td> Northern Season </td>
 	<td> Length (days) </td>
@@ -235,7 +250,7 @@ astro_seasons_table.innerHTML = `
 	<td> ${seasons[1][0]} </td>
 	<td> Autumn </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Summer  </td>
 	<td> ${seasons[1][1]} </td>
 	<td> Winter </td>
@@ -245,7 +260,7 @@ astro_seasons_table.innerHTML = `
 	<td> ${seasons[1][2]} </td>
 	<td> Spring </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Winter </td>
 	<td> ${seasons[1][3]} </td>
 	<td> Summer  </td>
@@ -258,7 +273,7 @@ body.appendChild(astro_seasons_table);
 const solar_seasons_table = document.createElement("table");
 solar_seasons_table.innerHTML = `
 <thead>
-<tr> <td colspan="3"> Lengths of Solar Seasons </td> </tr>
+<tr  class="title"> <td colspan="3"> Lengths of Solar Seasons </td> </tr>
 <tr> 
 	<td> Northern Season </td>
 	<td> Length (days) </td>
@@ -271,7 +286,7 @@ solar_seasons_table.innerHTML = `
 	<td> ${seasons[2][0]} </td>
 	<td> Autumn </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Summer  </td>
 	<td> ${seasons[2][1]} </td>
 	<td> Winter </td>
@@ -281,7 +296,7 @@ solar_seasons_table.innerHTML = `
 	<td> ${seasons[2][2]} </td>
 	<td> Spring </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Winter </td>
 	<td> ${seasons[2][3]} </td>
 	<td> Summer  </td>
@@ -296,7 +311,7 @@ var precessions = calculate_precessions(m);
 const precession_table = document.createElement("table");
 precession_table.innerHTML = `
 <thead>
-<tr> <td colspan="3"> Precession Properties </td> </tr>
+<tr  class="title"> <td colspan="3"> Precession Properties </td> </tr>
 <tr> 
 	<td> Property </td>
 	<td> Value </td>
@@ -309,7 +324,7 @@ precession_table.innerHTML = `
 	<td> ${precessions[0]} </td>
 	<td> days / rev </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Lunar Apsidal Precession </td>
 	<td> ${precessions[1]} </td>
 	<td> days / rev </td>
@@ -319,7 +334,7 @@ precession_table.innerHTML = `
 	<td> ${precessions[2]} </td>
 	<td> days </td>
 </tr>
-<tr> 
+<tr class='even'> 
 	<td> Draconic Month </td>
 	<td> ${precessions[3]} </td>
 	<td> days </td>
@@ -334,35 +349,97 @@ precession_table.innerHTML = `
 body.appendChild(precession_table);
 
 const eclipse_cycle_table = document.createElement("table");
-eclipse_cycle_table.innerHTML = `
+text = `
 <thead>
-<tr> <td colspan="4"> Eclipse Cycles </td> </tr>
+<tr  class="title"> <td colspan="5"> Eclipse Cycles </td> </tr>
 <tr> 
-	<td> Half-EY : SM </td>
-	<td> Delta xi (degrees) </td>
-	<td> Years </td>
-	<td> A.M. </td>
+	<td> SM : half-DM </td>
+	<td> Delta xi (deg) </td>
+	<td> Planet Years </td>
+	<td> Anom. Months </td>
+	<td> Node </td>
 </tr>
 
 </thead>
 <tbody>
 `;
 
-var eclipse_cycles = dec_to_frac(moon_synodic/(precessions[4]/2));
+var eclipse_cycles = dec_to_frac((precessions[3]/2) / moon_synodic , 15);
 for (let i = 0; i < eclipse_cycles.length; i++) {
-	let quotient = eclipse_cycles[i][1] * moon_synodic / precessions[4];
+	let quotient = eclipse_cycles[i][0] * moon_synodic / precessions[4];
 	let error = (quotient - Math.floor(quotient)) * 360;
-	error = Math.min(error, 360 - error);
-	error = Math.min(error, Math.abs(error-180), Math.abs(180-error));
-	let text = "<tr> <td>" + eclipse_cycles[i][0].toString() + " : " + eclipse_cycles[i][1].toString() + "</td> <td>" + error.toString() + "</td> ";
-	text += "<td>" + (eclipse_cycles[i][1] * moon_synodic / earth_period).toString() + "</td> <td>" + (eclipse_cycles[i][1] * moon_synodic / precessions[2]).toString() + "</td> </tr>";
-	eclipse_cycle_table.innerHTML += text;
+	let error_2 = (quotient - Math.floor(quotient) - 1) * 360;
+	let error_3 = (quotient - Math.floor(quotient) - 0.5) * 360;
+	if (Math.abs(error_2) < Math.abs(error)) {
+		error = error_2;
+	}
+	if (Math.abs(error_3) < Math.abs(error)) {
+		error = error_3;
+	}
+	text += "<tr "
+	if (i % 2 == 1) {
+		text += "class='even'"
+	}
+	text += "> <td>" + eclipse_cycles[i][0].toString() + " : " + eclipse_cycles[i][1].toString() + "</td> <td>" + error.toString() + "</td> ";
+	text += "<td>" + (eclipse_cycles[i][0] * moon_synodic / earth_period).toString() + "</td> <td>" + (eclipse_cycles[i][0] * moon_synodic / precessions[2]).toString() + "</td> ";
+	if (eclipse_cycles[i][1] % 2 == 0) {
+		text += "<td> Same </td> </tr>"
+	}
+	else {
+		text += "<td> Alternate </td> </tr>"
+	}
+	eclipse_cycle_table.innerHTML = text;
 }
 
 eclipse_cycle_table.innerHTML += "</tbody>";
 body.appendChild(eclipse_cycle_table);
 
-body.innerHTML += "<p> Eclipse cycles: " + dec_to_frac(moon_synodic/(precessions[4]/2)).join(" or ") + " half-E.Y./S.M.</p>";
+const eclipse_seasons_table = document.createElement("table");
+eclipse_seasons = calculate_eclipse_seasons();
+eclipse_seasons_table.innerHTML = `
+<thead>
+<tr  class="title"> <td colspan="4"> Mean Eclipse Conditions </td> </tr>
+<tr> 
+	<td> Eclipse Type </td>
+	<td> Beta less than (deg) </td>
+	<td> Xi less than (deg) </td>
+	<td> Season Length (days) </td>
+</tr>
+</thead>
+<tbody>
+<tr> 
+	<td> Partial Solar </td>
+	<td> ${eclipse_seasons[0][0] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][0] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][0] / (pi / earth_period)} </td>
+</tr>
+<tr class='even'> 
+	<td> Central Solar </td>
+	<td> ${eclipse_seasons[0][1] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][1] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][1] / (pi / earth_period)} </td>
+</tr>
+<tr> 
+	<td> Penumbral Lunar </td>
+	<td> ${eclipse_seasons[0][2] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][2] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][2] / (pi / earth_period)} </td>
+</tr>
+<tr class='even'> 
+	<td> Partial Lunar </td>
+	<td> ${eclipse_seasons[0][3] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][3] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][3] / (pi / earth_period)} </td>
+</tr>
+<tr> 
+	<td> Total Lunar </td>
+	<td> ${eclipse_seasons[0][4] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][4] * 180 / pi} </td>
+	<td> ${eclipse_seasons[1][4] / (pi / earth_period)} </td>
+</tr>
+</tbody>
+`;
+body.appendChild(eclipse_seasons_table);
 
 function period(mass1, mass2, sma) {
 	return Math.sqrt(4 * pi * pi * sma * sma * sma / (G * (mass1 + mass2))) / 3600 / day;
@@ -372,8 +449,8 @@ function surface_gravity(M, R) {
 	return G * M / R / R * 1000;
 }
 
-function dec_to_frac(number) {
-	let num_approxs = 10;
+function dec_to_frac(number, n) {
+	let num_approxs = n;
 	let decimal = number - Math.floor(number);
 	let denoms = [];
 	let fracs = [];
@@ -454,4 +531,28 @@ function calculate_precessions(m) {
 	let draconic = -nodal * moon_period / (-nodal + moon_period);
 	let eclipse_year = -nodal * earth_period / (-nodal + earth_period);
 	return [nodal, apsidal , anomalistic, draconic, eclipse_year];
+}
+
+function calculate_eclipse_seasons() {
+	let moon_semidiameter = Math.asin(moon_radius / moon_sma);
+	let sun_semidiameter = Math.asin(sun_radius / earth_sma);
+	let moon_parallax = Math.asin(earth_radius / moon_sma);
+	let sun_parallax = Math.asin(earth_radius / earth_sma);
+	let moon_movement = 2 * pi / moon_period;
+	let sun_movement = 2 * pi / earth_period;
+	let q = moon_movement / sun_movement;
+	let i_prime = Math.atan(q / (q - 1) * Math.tan(moon_inclination));
+
+	let partial_solar = 1/Math.cos(i_prime) * (moon_semidiameter + sun_semidiameter + moon_parallax - sun_parallax);
+	let central_solar = 1/Math.cos(i_prime) * (moon_semidiameter - sun_semidiameter + moon_parallax - sun_parallax);
+	let penumbral_lunar = 1/Math.cos(i_prime) * (moon_semidiameter + sun_semidiameter + moon_parallax + sun_parallax);
+	let partial_lunar = 1/Math.cos(i_prime) * (moon_semidiameter - sun_semidiameter + moon_parallax - sun_parallax);
+	let total_lunar = 1/Math.cos(i_prime) * (-moon_semidiameter - sun_semidiameter + moon_parallax - sun_parallax);
+	
+	let beta_array = [partial_solar, central_solar, penumbral_lunar, partial_lunar, total_lunar];
+	let xi_array = [];
+	for (let i = 0; i < beta_array.length; i++) {
+		xi_array.push(Math.asin(Math.tan(beta_array[i]) / Math.tan(moon_inclination)));
+	}
+	return [beta_array, xi_array];
 }
